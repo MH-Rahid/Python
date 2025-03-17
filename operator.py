03acb990de0bd750092d5afe9881de586fb132d2
MH-Rahid <mhrahid112@gmail.com>
2025-03-18 01:34:49 +0800

    added operators

---
+++ operator.py
@@ -0,0 +1,48 @@
+# arithmetic operators
+print("--- arithmetic operators  --- ")
+a = 8
+b = 7
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a ** b)
+
+# Assignment operators
+print("--- Assignment operators ---")
+
+print( )
+x = 10
+print(x)
+x += 5
+print(x)
+x -= 5
+print(x)
+x *= 5
+print(x)
+x /= 5
+print(x)
+x **= 5
+print(x)
+
+# Comparison operators
+print("--- Comparison operators ---")
+print( )
+y = 8
+z = 9
+
+print(y == z)
+print(y > z)
+print(y < z)
+print(y >= z)
+print(y <= z)
+
+# logical operators
+print("--- Logical operators ---")
+print( )
+a = True
+b = True
+
+print(a and b)
+print(a or b)
+print(not(a and b))
